6ed68bec2b2a8266cf65cc9eee03980c231c4fd4
ThamizhAnban <thamizhanban2006@gmail.com>
2025-05-06 16:35:44 +0530

    Database connected and api calls done

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-
+const mongoose = require('mongoose');
 const PORT = process.env.PORT;
-app.use(express.json())
+const uri = process.env.uri;
+const routes = require('./routes')
 app.get('/',(req,res)=>{
     res.send('Hello');
 })
+
+
+mongoose.connect(uri)
+.then(()=>console.log("MongoDB connected successfully!"))
+.catch((err)=>console.log(err));
+
+
+app.use(express.json())
+app.use('/api',routes)
 
 app.listen(PORT,()=>{
     console.log(`This server is running in : http://localhost:${PORT}`)

a8f54b1315eef4a8d9824cf69ae650f51430383a
Thamizhanban2006 <thamizhanban2006@gmail.com>
2025-06-24 13:02:11 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,49 @@
-const express = require('express')
-const app = express();
-const port = 3000
-const routes = require('./routes')
-const mongoose = require('mongoose');
 require('dotenv').config();
-const uri = process.env.uri;
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
-app.use(cors());
 const http = require('http');
 const { Server } = require('socket.io');
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://codebudd.netlify.app'],
+  credentials: true
+}));
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: '*', 
     methods: ['GET', 'POST']
   }
 });
 
 io.on('connection', (socket) => {
-  console.log('New socket connection');
-
-  socket.on('joinRoom', (answerId) => {
-    socket.join(answerId);
-  });
-
-  socket.on('sendMessage', ({ AnswerId, sender, text }) => {
-    io.to(AnswerId).emit('receiveMessage', { AnswerId, sender, text });
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+  console.log('Socket connected');
+  socket.on('joinRoom', (answerId) => socket.join(answerId));
+  socket.on('sendMessage', (msg) => io.to(msg.AnswerId).emit('receiveMessage', msg));
+  socket.on('disconnect', () => console.log('Socket disconnected'));
 });
 
-
-
-app.get('/',(req,res)=>{
-    res.send('Hi guys!')
-});
-
+const uri = process.env.uri;
+if (!uri) {
+  console.error("MongoDB URI not set in .env file!");
+  process.exit(1);
+}
 mongoose.connect(uri)
-.then(()=>console.log("MongoDB connected successfully!"))
-.catch((err)=>console.log(err));
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
 app.use(express.json());
-app.use('/api',routes)
+app.use('/api', routes);
 
-app.listen(port,()=>{
-    console.log(`http://localhost:${port}`);
-})
+app.get('/', (req, res) => res.send('Hi guys!'));
+
+server.listen(port, () => {
+  console.log(` Server running at http://localhost:${port}`);
+});
